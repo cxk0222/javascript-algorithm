3a7bf226ac76f6875c77822ce2ce6210ab28a41b
cxk0222 <642192014@qq.com>
2025-02-08 16:34:51 +0800

    实现迭代阶乘和递归阶乘

---
+++ src/other/factorial.js
@@ -0,0 +1,16 @@
+function factorialIterative(number) {
+  if (number < 0) return
+  let total = 1
+  for (let i = number; i > 0; i--) {
+    total = total * i
+  }
+  return total
+}
+
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1
+  } else {
+    return number * factorial(number - 1)
+  }
+}
