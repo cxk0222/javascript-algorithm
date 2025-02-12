acf0aa431be4d7358ab4e113fda38b00e64f7aa9
cxk0222 <642192014@qq.com>
2025-02-12 11:37:30 +0800

    实现BinarySearchTree中序遍历

--- src/data-structures/binary-search-tree.js
+++ src/data-structures/binary-search-tree.js
@@ -30,11 +30,40 @@ export default class BinarySearchTree {
       }
     }
   }
+
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback)
+  }
+
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      // console.log('inOrderTraverseNode left', node.left?.key)
+      this.inOrderTraverseNode(node.left, callback)
+      // console.log('inOrderTraverseNode callback', node)
+      callback(node.key)
+      // console.log('inOrderTraverseNode right', node.right?.key)
+      this.inOrderTraverseNode(node.right, callback)
+    }
+  }
 }
 
-var tree = new BinarySearchTree()
-tree.insert(100)
-tree.insert(10)
+const tree = new BinarySearchTree()
 tree.insert(11)
+tree.insert(7)
+tree.insert(15)
+tree.insert(5)
+tree.insert(3)
 tree.insert(9)
-console.log('tree', tree)
+tree.insert(8)
+tree.insert(10)
+tree.insert(13)
+tree.insert(12)
+tree.insert(14)
+tree.insert(20)
+tree.insert(18)
+tree.insert(25)
+tree.insert(6)
+// console.log('tree', tree)
+
+const printNode = (value) => console.log(value)
+tree.inOrderTraverse(printNode)
